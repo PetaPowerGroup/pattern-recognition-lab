782b38b452899b7c1d3d0f611e6dc9b867a0fb0c
mat1987 <>
2009-12-16 12:02:04 +0000

    Napravio sam novi kod za izlučivanje značajki, jer nisam mogao naći grešku u starom.

--- kod/SVM-Face_Recognition/IzlucivanjeZnacajki.cpp
+++ kod/SVM-Face_Recognition/IzlucivanjeZnacajki.cpp
@@ -19,19 +19,21 @@ using namespace std;
 
 
 std::vector<double> IzlucivanjeZnacajki::izluciZnacajke(Uzorak uzorak) {//ova povratna vrijednost bi mi pasala, za pozivanje od tamo od kud sam mislio
-
+/*
 	// integralna slika - kako bi manje puta pristupal slici i na taj naèin ubrzali ovaj postupak
 	vector< vector<int> > integrirana_slika(uzorak.getVisina()+1);
 	//Uzorak integralna_slika(uzorak.getSirina(), uzorak.getVisina());
 	int zbroj = 0;
 	
 	vector<int> *redak_integralne_slike=new vector<int>(uzorak.getSirina()+1);
+	vector<int> *pomocni=new vector<int>(uzorak.getSirina()+1);
 	for (int i = 1; i <= uzorak.getSirina(); i++)
 	{
 		zbroj += uzorak.getPixel(i-1); 
 		//integralna_slika.setPixel(i, zbroj);
 		//redak_integralne_slike->push_back(zbroj);
-		redak_integralne_slike->at(i)=zbroj;
+		redak_integralne_slike->at(i-1) = zbroj;
+		pomocni->at(i-1) = uzorak.getPixel(i-1);
 		//cout << "i je: " << i<<endl;
 	}
 	integrirana_slika.at(1)=*redak_integralne_slike;
@@ -101,9 +103,47 @@ std::vector<double> IzlucivanjeZnacajki::izluciZnacajke(Uzorak uzorak) {//ova po
 	// Uzorak integralna_slika potrebno je izbrisati, ali u klasi uzorak nema destruktora. 
 
 	//praznjenje memorije
-	for(int i=0; i<(int)integrirana_slika.size(); ++i)
+	for(int i=0; i<(int)integrirana_slika.size(); ++i) 
 		integrirana_slika.at(i).~vector();
 	integrirana_slika.~vector();
+
+	return izracunateZnacajke;
+*/
+	std::vector<double> izracunateZnacajke;//=std::vector<double>();//u ovu listu trpaš vrijednosti znaèajki, kako ih izraèunavaš
+
+	int sirina_okna = 8;  
+	int visina_okna = 8;  // ako je slika velika 64x64 imamo 64 znaèajke, može se poveæavati ili smanjivati ovisno o rezultatima 
+
+	double tamni, svijetli;
+
+	for (int i = 0; i < (int)(uzorak.getVisina() - visina_okna); i += visina_okna)  
+		{
+			for (int j = 0; j < (int)(uzorak.getSirina() - sirina_okna); j += sirina_okna) 
+			{
+				tamni = 0;
+				svijetli = 0;
+				for (int x = i; x < i + visina_okna; x++)
+				{
+					for (int y = j; y < j + sirina_okna; y++) 
+					{
+						if (y < (j + sirina_okna/2)) 
+						{
+							tamni += uzorak.getPixel(x,y);
+						}
+						else 
+						{
+							svijetli += uzorak.getPixel(x,y);
+						}
+					}
+				}
+			
+				double rezultat=(svijetli-tamni+sirina_okna*visina_okna/2*255)/(sirina_okna*visina_okna*255);
+				izracunateZnacajke.push_back(rezultat); // i dobili smo znaèajku
+				assert (rezultat<=1.0 && rezultat>=0.0);
+			} 
+		} 
+
+	
 
 	return izracunateZnacajke;
 }
