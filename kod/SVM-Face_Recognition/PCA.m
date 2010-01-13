5ab0a26c198dc165ec558443ad7f5f313d7353a7
asimic <>
2010-01-13 23:28:26 +0000

    evo, istestirano je na gulićevom txt-u lice i radi, dok za lica-sva ne radi :S. Argumenti su početni broj značajki i željeni broj značajki. Broj uzoraka je zadan u varijabli k. Izlazna datoteka je r_uzorak.txt

--- kod/SVM-Face_Recognition/PCA.m
+++ kod/SVM-Face_Recognition/PCA.m
@@ -1,25 +1,31 @@
 % uzorak je matrica uèitana iz c++a
 function pca(a,n);
 
-k=2352;
+k=20;
 %uèitavanje iz txt fajla
 %pretpostavio sam da je broj uzoraka 2400(8x300)
 uzorak = zeros(a,k);
-fid = fopen('svm/lica-sva.txt','r');
+broj = zeros(a,k);
+c=zeros(2,k);
+fid = fopen('svm/lice.txt','r');
 for i=1:k
-fscanf(fid, '%d');
-    for j=1:a
+    c(:,i) = fscanf(fid, '%d %d');
+    broj(1,i)=c(2,i);
+    data = fscanf(fid, ':%f32');
+    uzorak(1,i)=data;
+    for j=2:a
         data = fscanf(fid, '%d:%f32');
         uzorak(j,i)=data(2);
+        broj(j,i)=data(1);
     end
-fscanf(fid, '\n');
+    fscanf(fid, '\n');
 end
 fclose(fid);
 
 %size(uzorak)=4096x2352
 cov = zeros(a,a);
 for i=1:k
-    m = uzorak(:,i)-mean(mean(uzorak(:,i))).*ones(2,1);
+    m = uzorak(:,i)-mean(mean(uzorak(:,i))).*ones(a,1);
     cov = cov + m*m';
 end
 cov = cov/8;
@@ -39,9 +45,10 @@ r_uzorak = e'*uzorak;
 
 fid = fopen('svm/r_uzorak.txt','w');
 for i=1:k
-fprintf(fid, '%d ', floor((i-1)/8)+1);
+fprintf(fid, '%d ', c(1,i));
+%floor((i-1)/8)+1
     for j=1:n
-        fprintf(fid, '%d:%f ',j, r_uzorak(j,i));
+        fprintf(fid, '%d:%f ',broj(j,i), r_uzorak(j,i));
     end
     fprintf(fid, '\r\n');
 end
